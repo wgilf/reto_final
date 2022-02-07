c72467395479dc1dd39c9ad46d54fa39d3862730
wgilf <98993911+wgilf@users.noreply.github.com>
2022-02-07 10:28:11 -0500

    Update reto.py

--- reto.py
+++ reto.py
@@ -26,3 +26,31 @@ print(Fore.GREEN + "||||||||||||   ||||||||||   |||||   ||||   |||||      ||  | 
 print(Fore.GREEN + "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 print(Fore.GREEN + "                                                          WELCOME")
 print()
+qwe=True
+while (qwe==True):
+    ssdf=0
+    print(Fore.MAGENTA + "Para jugar introdusca el numero 1 -Para ver la lista de ganadores introdusca el numero 2")
+    try:
+        ppl=int(input("="))
+        if(ppl==1):
+            qwe=False
+        elif(ppl==2):
+            with open(r'bus.json', 'r') as f:
+                json_data = json.load(f)
+            for hh in range(len(json_data)):
+                print(json_data[hh])
+
+            
+            ssdf=1
+            exit()
+        else:
+            print(Fore.RED + "NUMERO INVALIDADO")
+    except:
+        if(ssdf==1):
+            print(Fore.RED + "Fin")
+        else:
+            print("no se admiten letras")
+    if(ssdf==1):
+        exit()
+        
+        
