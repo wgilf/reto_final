c4d0db02f4841ebeb9288645160967d0a08e652f
wgilf <98993911+wgilf@users.noreply.github.com>
2022-02-07 10:23:04 -0500

    Update reto.py

--- reto.py
+++ reto.py
@@ -1,3 +1,8 @@
 #Wilson David Gil-Karen Johana Farfan Castro
 import random
 import json
+base=(" ","1","2","3","4","5","6","7","8","9")
+tablero=[" ","1","2","3","4","5","6","7","8","9"]
+primer_player="x"
+segundo_player="O"
+listado=[]
